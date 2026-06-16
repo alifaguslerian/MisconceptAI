c98bfe77b24a4406648303db55dd76f3eedd2dd3
alifaguslerian <alifaguslerian512@gmail.com>
2026-06-16 17:24:22 +0700

    delete code navigate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, NavLink, useNavigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route, NavLink,} from "react-router-dom"
 import { useEffect, useState } from "react"
 import axios from "axios"
 import SessionNew from "./pages/SessionNew"
